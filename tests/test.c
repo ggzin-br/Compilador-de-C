5636b0afd68d1a95227c50fd1148d027277854fb
Pedro Garcia Vilanova <garciavp@protonmail.com>
2025-06-25 18:58:59 -0300

    código de referência do lab-3 disponível na plataforma

---
+++ tests/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main() { 
+    int a = 0x011;
+    float b = 0b1000;
+    double c = 50+20+10;
+    float d = 0x400;
+    // teste de comentario 
+    return NULL; 
+}
