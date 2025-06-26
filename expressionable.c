8084736882cc961d014fb83a6949308e0da7fd85
Pedro Garcia Vilanova <garciavp@protonmail.com>
2025-06-26 18:08:55 -0300

    envio do lab 4 ainda sem funcionar por algum motivo extraordinário... deve ser algum problema no parser.c

---
+++ expressionable.c
@@ -0,0 +1,19 @@
+#include "compiler.h"
+
+// Formato: {operator1, operator2, operator3, ..., NULL}
+struct expressionable_op_precedence_group op_precedence[TOTAL_OPERADOR_GROUPS] = {
+    {.operators={"++", "--", "()", "[]", "(", "[", ".", "->", NULL},.associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"*", "/", "%", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"+", "-", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"<<", ">>", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"<", "<=", ">", ">=", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"==", "!=", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"&", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"^", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"|", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"&&", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"||", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH},
+    {.operators={"?", ":", NULL}, .associativity=ASSOCIATIVITY_RIGHT_TO_LEFT}, //D->E
+    {.operators={"=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", "&=", "^=", "|=", NULL},.associativity=ASSOCIATIVITY_RIGHT_TO_LEFT}, //D->E
+    {.operators={",", NULL}, .associativity=ASSOCIATIVITY_LEFT_TO_RIGTH}
+};
